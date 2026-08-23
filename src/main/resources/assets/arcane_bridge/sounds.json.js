1f2b685260c53bae55fbf0edcfca66d1d2eb363d
golden <arcane_assamblage@mail.ru>
2026-08-23 21:37:00 +0300

    ⚡ Auto-Patch: sounds.json.js - file created/overwritten

---
+++ src/main/resources/assets/arcane_bridge/sounds.json.js
@@ -0,0 +1,20 @@
+{
+  "guide.greeting_01": {
+    "category": "voice",
+    "sounds": [
+      "arcane_bridge:guide/greeting_01"
+    ]
+  },
+  "guide.progression_nether": {
+    "category": "voice",
+    "sounds": [
+      "arcane_bridge:guide/progression_nether"
+    ]
+  },
+  "guide.resonance_warning": {
+    "category": "voice",
+    "sounds": [
+      "arcane_bridge:guide/resonance_warning"
+    ]
+  }
+}
