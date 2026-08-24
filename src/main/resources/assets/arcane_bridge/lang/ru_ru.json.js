9232d516e128d37c65add6c97939d0fb43b61e9a
golden <arcane_assamblage@mail.ru>
2026-08-24 22:37:23 +0300

    ⚡ Auto-Patch: ru_ru.json.js - applied 1 patches

--- src/main/resources/assets/arcane_bridge/lang/ru_ru.json.js
+++ src/main/resources/assets/arcane_bridge/lang/ru_ru.json.js
@@ -1,6 +1,6 @@
 {
-  "item.arcane_bridge.guide_core": "Проекционный модуль АРК-0",
-  "entity.arcane_bridge.arcane_guide": "Архи-Оператор АРК-0",
+  "item.arcane_bridge.guide_core": "Ядро Мастера Резонанса",
+  "entity.arcane_bridge.arcane_guide": "Мастер Резонанса",
   "key.categories.arcane_bridge": "Arcane Bridge",
   "key.arcane_bridge.hud_menu": "Меню интерфейса визора"
 }
