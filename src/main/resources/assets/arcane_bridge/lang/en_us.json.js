97999b1c051921f125027a59e075a678c2c61db9
golden <arcane_assamblage@mail.ru>
2026-08-24 23:31:28 +0300

    гид 4.1 - Мастер Резонанса попытка озвучки

--- src/main/resources/assets/arcane_bridge/lang/en_us.json.js
+++
@@ -1,6 +0,0 @@
-{
-  "item.arcane_bridge.guide_core": "Resonance Master Core",
-  "entity.arcane_bridge.arcane_guide": "Master of Resonance",
-  "key.categories.arcane_bridge": "Arcane Bridge",
-  "key.arcane_bridge.hud_menu": "Visor HUD Menu"
-}
--- src/main/resources/assets/arcane_bridge/lang/ru_ru.json.js
+++
@@ -1,6 +0,0 @@
-{
-  "item.arcane_bridge.guide_core": "Ядро Мастера Резонанса",
-  "entity.arcane_bridge.arcane_guide": "Мастер Резонанса",
-  "key.categories.arcane_bridge": "Arcane Bridge",
-  "key.arcane_bridge.hud_menu": "Меню интерфейса визора"
-}
