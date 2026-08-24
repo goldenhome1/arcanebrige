e21bbdb8c0a39563b3c8c4aea233ea563b380a66
golden <arcane_assamblage@mail.ru>
2026-08-24 23:49:01 +0300

    ⚡ Auto-Patch: GuideDialogueScreen.java - applied 1 patches

--- src/main/java/com/example/arcanebridge/client/gui/GuideDialogueScreen.java
+++ src/main/java/com/example/arcanebridge/client/gui/GuideDialogueScreen.java
@@ -7,6 +7,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
