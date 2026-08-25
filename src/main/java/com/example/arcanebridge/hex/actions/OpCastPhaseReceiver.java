62e03df6454d0074014bcbfc0cfd2340c5688db5
golden <arcane_assamblage@mail.ru>
2026-08-25 12:59:52 +0300

    ⚡ Auto-Patch: OpCastPhaseReceiver.java - applied 1 patches

--- src/main/java/com/example/arcanebridge/hex/actions/OpCastPhaseReceiver.java
+++ src/main/java/com/example/arcanebridge/hex/actions/OpCastPhaseReceiver.java
@@ -21,108 +21,105 @@ import java.util.List;
 
 public class OpCastPhaseReceiver implements Action {
 
-    @NotNull
-    @Override
-    public OperationResult operate(@NotNull CastingEnvironment env, @NotNull CastingImage image, @NotNull SpellContinuation continuation) {
-        List<Iota> stack = new ArrayList<>();
-        try {
-            stack = (List<Iota>) image.getClass().getMethod("getStack").invoke(image);
-        } catch (Exception ignored) {}
 
-        List<Iota> newStack = new ArrayList<>(stack);
+    @NotNull
+
+    @Override
+
+    public OperationResult operate(@NotNull CastingEnvironment env, @NotNull CastingImage image, @NotNull SpellContinuation continuation) {
+
+        List<Iota> newStack = new ArrayList<>(image.getStack());
+
         if (newStack.size() < 2) {
+
             newStack.add(new NullIota());
-            return buildResult(image, continuation, newStack);
+
+            return buildResult(image, newStack);
+
         }
+
 
         Iota channelIota = newStack.remove(newStack.size() - 1);
+
         Iota targetIota = newStack.remove(newStack.size() - 1);
 
+
         if (!(targetIota instanceof Vec3Iota posIota) || !(channelIota instanceof DoubleIota numIota)) {
+
             newStack.add(new NullIota());
-            return buildResult(image, continuation, newStack);
+
+            return buildResult(image, newStack);
+
         }
+
 
         Vec3 targetVec = posIota.getVec3();
+
         BlockPos targetPos = BlockPos.containing(targetVec);
+
         double channelId = numIota.getDouble();
+
         ServerLevel level = (ServerLevel) env.getWorld();
 
+
         // 1. Проверяем вал Create
+
         if (!KineticValidationHelper.isKineticBlock(level, targetPos)) {
+
             newStack.add(new NullIota());
-            return buildResult(image, continuation, newStack);
+
+            return buildResult(image, newStack);
+
         }
+
 
         // 2. Регистрируем вал как Приемник
+
         PhaseNetworkManager manager = PhaseNetworkManager.get(level.getServer());
+
         manager.registerReceiver(channelId, level.dimension(), targetPos);
 
+
         // Визуальный и звуковой отклик каста
+
         double px = targetPos.getX() + 0.5D;
+
         double py = targetPos.getY() + 0.5D;
+
         double pz = targetPos.getZ() + 0.5D;
+
         level.sendParticles(net.minecraft.core.particles.ParticleTypes.PORTAL, px, py, pz, 24, 0.4, 0.4, 0.4, 0.15);
+
         level.sendParticles(net.minecraft.core.particles.ParticleTypes.WITCH, px, py, pz, 12, 0.3, 0.3, 0.3, 0.05);
+
         level.playSound(null, targetPos, net.minecraft.sounds.SoundEvents.AMETHYST_BLOCK_RESONATE, net.minecraft.sounds.SoundSource.BLOCKS, 0.9F, 1.4F);
 
+
         newStack.add(new DoubleIota(channelId));
-        return buildResult(image, continuation, newStack);
+
+        return buildResult(image, newStack);
+
     }
 
-        private OperationResult buildResult(CastingImage image, SpellContinuation continuation, List<Iota> newStack) {
-        CastingImage nextImage = image;
-        try {
-            for (java.lang.reflect.Method m : image.getClass().getMethods()) {
-                if (("copy".equals(m.getName()) || "withStack".equals(m.getName())) && m.getParameterCount() > 0) {
-                    if (List.class.isAssignableFrom(m.getParameterTypes()[0])) {
-                        Object[] args = new Object[m.getParameterCount()];
-                        args[0] = newStack;
-                        Class<?>[] pTypes = m.getParameterTypes();
-                        for (int i = 1; i < pTypes.length; i++) {
-                            for (java.lang.reflect.Field f : image.getClass().getDeclaredFields()) {
-                                if (pTypes[i].isAssignableFrom(f.getType())) {
-                                    f.setAccessible(true);
-                                    args[i] = f.get(image);
-                                    break;
-                                }
-                            }
-                        }
-                        nextImage = (CastingImage) m.invoke(image, args);
-                        break;
-                    }
-                }
-            }
-        } catch (Throwable ignored) {}
 
-        for (java.lang.reflect.Constructor<?> c : OperationResult.class.getConstructors()) {
-            try {
-                c.setAccessible(true);
-                Class<?>[] pTypes = c.getParameterTypes();
-                Object[] args = new Object[pTypes.length];
-                for (int i = 0; i < pTypes.length; i++) {
-                    if (CastingImage.class.isAssignableFrom(pTypes[i])) {
-                        args[i] = nextImage;
-                    } else if (SpellContinuation.class.isAssignableFrom(pTypes[i])) {
-                        args[i] = continuation;
-                    } else if (List.class.isAssignableFrom(pTypes[i])) {
-                        args[i] = new ArrayList<>();
-                    } else if (pTypes[i] == boolean.class) {
-                        args[i] = false;
-                    } else if (pTypes[i] == int.class) {
-                        args[i] = 0;
-                    } else if (pTypes[i] == long.class) {
-                        args[i] = 0L;
-                    } else {
-                        args[i] = null;
-                    }
-                }
-                OperationResult res = (OperationResult) c.newInstance(args);
-                if (res != null) {
-                    return res;
-                }
-            } catch (Throwable ignored) {}
-        }
-        return null;
+    private OperationResult buildResult(CastingImage image, List<Iota> newStack) {
+
+        CastingImage nextImage = image.copy(
+
+                newStack,
+
+                image.getParenChildren(),
+
+                image.getEscapeNext(),
+
+                image.getOpsConsumed(),
+
+                image.getUserData()
+
+        );
+
+        return new OperationResult(nextImage, new ArrayList<>());
+
     }
+
 }
