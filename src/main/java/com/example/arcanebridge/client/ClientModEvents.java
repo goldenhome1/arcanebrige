16a5d9413551a2659027c854026394f96e4d3df2
golden <arcane_assamblage@mail.ru>
2026-08-25 19:43:31 +0300

    ⚡ Auto-Patch: ClientModEvents.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/ClientModEvents.java
+++ src/main/java/com/example/arcanebridge/client/ClientModEvents.java
@@ -1,8 +1,10 @@
 package com.example.arcanebridge.client;
 
 import com.example.arcanebridge.ArcaneBridge;
+import com.example.arcanebridge.client.render.PhaseRelayRenderer;
 import com.example.arcanebridge.client.renderer.ArcaneGuideRenderer;
 import com.example.arcanebridge.entity.ModEntities;
+import com.example.arcanebridge.registry.ModBlockEntities;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,5 +16,6 @@ public class ClientModEvents {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ModEntities.ARCANE_GUIDE.get(), ArcaneGuideRenderer::new);
+        event.registerBlockEntityRenderer(ModBlockEntities.PHASE_RELAY.get(), PhaseRelayRenderer::new);
     }
 }
