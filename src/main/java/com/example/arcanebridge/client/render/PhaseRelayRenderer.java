71e03ef28929c28d02501a5f3f7b500995c1110d
golden <arcane_assamblage@mail.ru>
2026-08-25 19:55:22 +0300

    ⚡ Auto-Patch: PhaseRelayRenderer.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/render/PhaseRelayRenderer.java
+++ src/main/java/com/example/arcanebridge/client/render/PhaseRelayRenderer.java
@@ -4,14 +4,11 @@ import com.example.arcanebridge.block.entity.PhaseRelayBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
-import com.simibubi.create.content.kinetics.base.RotatedPillarKineticBlock;
-import dev.engine_room.flywheel.api.visualization.VisualizationManager;
 import net.createmod.catnip.render.CachedBuffers;
 import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class PhaseRelayRenderer extends KineticBlockEntityRenderer<PhaseRelayBlockEntity> {
@@ -23,18 +20,14 @@ public class PhaseRelayRenderer extends KineticBlockEntityRenderer<PhaseRelayBlo
     @Override
     protected void renderSafe(PhaseRelayBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer,
                               int light, int overlay) {
-        if (VisualizationManager.supportsVisualization(be.getLevel())) return;
-
+        // Рендерим вал принудительно (без отсечки Flywheel)
         BlockState state = be.getBlockState();
         renderRotatingBuffer(be, getRotatedModel(be, state), ms, buffer.getBuffer(RenderType.solid()), light);
     }
 
     @Override
     protected SuperByteBuffer getRotatedModel(PhaseRelayBlockEntity be, BlockState state) {
-        Direction.Axis axis = state.getValue(RotatedPillarKineticBlock.AXIS);
-        Direction facing = Direction.fromAxisAndDirection(axis, Direction.AxisDirection.POSITIVE);
-        
-        // Поворачиваем вертикальный вал AllPartialModels.SHAFT строго по направлению оси блока
-        return CachedBuffers.partialFacingVertical(AllPartialModels.SHAFT, state, facing);
+        // getRotatedModel суперкласса правильно ориентирует и вращает модель вала по оси блока
+        return CachedBuffers.partial(AllPartialModels.SHAFT, state);
     }
 }
