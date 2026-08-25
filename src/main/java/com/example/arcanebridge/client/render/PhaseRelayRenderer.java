7436b6c899081b6817c258723c375eaf0125f80c
golden <arcane_assamblage@mail.ru>
2026-08-25 15:59:24 +0300

    ⚡ Auto-Patch: PhaseRelayRenderer.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/render/PhaseRelayRenderer.java
+++ src/main/java/com/example/arcanebridge/client/render/PhaseRelayRenderer.java
@@ -4,11 +4,9 @@ import com.example.arcanebridge.block.entity.PhaseRelayBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
-import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer.KBlockEntityRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.world.level.block.state.BlockState;
 
 public class PhaseRelayRenderer extends KineticBlockEntityRenderer<PhaseRelayBlockEntity> {
 
@@ -19,14 +17,7 @@ public class PhaseRelayRenderer extends KineticBlockEntityRenderer<PhaseRelayBlo
     @Override
     protected void renderSafe(PhaseRelayBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer,
                               int light, int overlay) {
-        // 1. Нативный рендер вращающегося вала Create
-        if (be.getBlockState() != null) {
-            renderRotatingBuffer(be, getRotatedModel(be, be.getBlockState()), ms, buffer.getBuffer(RenderType.solid()), light);
-        }
-    }
-
-    @Override
-    protected com.simibubi.create.foundation.render.SuperByteBuffer getRotatedModel(PhaseRelayBlockEntity be, BlockState state) {
-        return com.simibubi.create.foundation.render.CachedBufferer.partialFacing(AllPartialModels.SHAFT, state);
+        // Отрисовка вала Create через встроенный стандартный метод KineticBlockEntityRenderer
+        standardShaft(be, ms, buffer, light);
     }
 }
