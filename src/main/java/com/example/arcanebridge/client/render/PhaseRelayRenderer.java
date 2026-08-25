20cc4833770e8d812f997f1bbeda91398e8c8819
golden <arcane_assamblage@mail.ru>
2026-08-25 20:01:26 +0300

    ⚡ Auto-Patch: PhaseRelayRenderer.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/render/PhaseRelayRenderer.java
+++ src/main/java/com/example/arcanebridge/client/render/PhaseRelayRenderer.java
@@ -1,16 +1,18 @@
 package com.example.arcanebridge.client.render;
 
+import com.example.arcanebridge.block.PhaseRelayBlock;
 import com.example.arcanebridge.block.entity.PhaseRelayBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
-import com.simibubi.create.content.kinetics.base.RotatedPillarKineticBlock;
 import net.createmod.catnip.render.CachedBuffers;
 import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class PhaseRelayRenderer extends KineticBlockEntityRenderer<PhaseRelayBlockEntity> {
 
@@ -22,14 +24,20 @@ public class PhaseRelayRenderer extends KineticBlockEntityRenderer<PhaseRelayBlo
     protected void renderSafe(PhaseRelayBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer,
                               int light, int overlay) {
         BlockState state = be.getBlockState();
+        if (state == null || !(state.getBlock() instanceof PhaseRelayBlock)) return;
+
+        // Рендерим и вращаем вал через нативный пайплайн Create
         renderRotatingBuffer(be, getRotatedModel(be, state), ms, buffer.getBuffer(RenderType.solid()), light);
     }
 
     @Override
     protected SuperByteBuffer getRotatedModel(PhaseRelayBlockEntity be, BlockState state) {
-        // Берем запеченную модель вала Create с правильной ориентацией оси (X, Y или Z)
-        BlockState shaftState = AllBlocks.SHAFT.get().defaultBlockState()
-                .setValue(RotatedPillarKineticBlock.AXIS, state.getValue(RotatedPillarKineticBlock.AXIS));
-        return CachedBuffers.block(shaftState);
+        Block shaftBlock = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("create", "shaft"));
+        if (shaftBlock != null) {
+            BlockState shaftState = shaftBlock.defaultBlockState()
+                    .setValue(PhaseRelayBlock.AXIS, state.getValue(PhaseRelayBlock.AXIS));
+            return CachedBuffers.block(shaftState);
+        }
+        return CachedBuffers.block(state);
     }
 }
