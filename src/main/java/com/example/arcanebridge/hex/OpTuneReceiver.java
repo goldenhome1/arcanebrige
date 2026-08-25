7de0f4352c2aef09757e4068ab725e94546d3e16
golden <arcane_assamblage@mail.ru>
2026-08-26 00:18:31 +0300

    ⚡ Auto-Patch: OpTuneReceiver.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/hex/OpTuneReceiver.java
+++ src/main/java/com/example/arcanebridge/hex/OpTuneReceiver.java
@@ -1,9 +1,9 @@
 package com.example.arcanebridge.hex;
 
+import at.petrak.hexcasting.api.casting.Action;
 import at.petrak.hexcasting.api.casting.ParticleSpray;
-import at.petrak.hexcasting.api.casting.RenderedSpell;
-import at.petrak.hexcasting.api.casting.castables.SpellAction;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
+import at.petrak.hexcasting.api.casting.eval.OperationResult;
 import at.petrak.hexcasting.api.casting.eval.vm.CastingImage;
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
 import at.petrak.hexcasting.api.casting.iota.DoubleIota;
@@ -11,6 +11,7 @@ import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.casting.iota.Vec3Iota;
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadBlock;
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota;
+import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughArgs;
 import at.petrak.hexcasting.api.misc.MediaConstants;
 import com.example.arcanebridge.block.PhaseRelayBlock;
 import com.example.arcanebridge.block.entity.PhaseRelayBlockEntity;
@@ -26,29 +27,33 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class OpTuneReceiver implements SpellAction {
+public class OpTuneReceiver implements Action {
 
     @Override
-    public int getArgc() {
-        return 2;
-    }
-
-    @Override
-    public SpellAction.Result execute(List<? extends Iota> args, CastingEnvironment ctx) {
-        if (!(args.get(0) instanceof Vec3Iota vecIota)) {
-            throw new MishapInvalidIota(args.get(0), 1, null);
-        }
-        if (!(args.get(1) instanceof DoubleIota numIota)) {
-            throw new MishapInvalidIota(args.get(1), 0, null);
+    public OperationResult operate(CastingEnvironment env, CastingImage image, SpellContinuation continuation) {
+        List<Iota> stack = new ArrayList<>(image.getStack());
+        if (stack.size() < 2) {
+            throw new MishapNotEnoughArgs(2, stack.size());
         }
 
-        Vec3 targetVec = vecIota.getVec3();
-        ctx.assertVecInRange(targetVec);
+        Iota numIota = stack.remove(stack.size() - 1);
+        Iota vecIota = stack.remove(stack.size() - 1);
+
+        if (!(vecIota instanceof Vec3Iota vIota)) {
+            throw new MishapInvalidIota(vecIota, 1, null);
+        }
+        if (!(numIota instanceof DoubleIota dIota)) {
+            throw new MishapInvalidIota(numIota, 0, null);
+        }
+
+        Vec3 targetVec = vIota.getVec3();
+        env.assertVecInRange(targetVec);
 
         BlockPos targetPos = BlockPos.containing(targetVec);
-        ServerLevel level = ctx.getWorld();
+        ServerLevel level = env.getWorld();
         BlockState targetState = level.getBlockState(targetPos);
 
         Block shaftBlock = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("create", "shaft"));
@@ -59,40 +64,27 @@ public class OpTuneReceiver implements SpellAction {
             throw new MishapBadBlock(targetPos, targetState.getBlock().getName());
         }
 
-        double channel = numIota.getDouble();
         long cost = isCreateShaft ? MediaConstants.SHARD_UNIT : MediaConstants.DUST_UNIT;
+        env.extractMedia(cost, false);
 
-        return new SpellAction.Result(
-                new Spell(targetPos, channel, isCreateShaft, targetState),
-                cost,
-                List.of(ParticleSpray.cloud(Vec3.atCenterOf(targetPos), 1.5D, 20))
-        );
-    }
+        double channel = dIota.getDouble();
 
-    private record Spell(BlockPos pos, double channel, boolean transform, BlockState oldState) implements RenderedSpell {
-        @Override
-        public void cast(CastingEnvironment ctx) {
-            ServerLevel level = ctx.getWorld();
+        if (isCreateShaft) {
+            Direction.Axis axis = targetState.hasProperty(PhaseRelayBlock.AXIS)
+                    ? targetState.getValue(PhaseRelayBlock.AXIS)
+                    : Direction.Axis.Y;
 
-            if (transform) {
-                Direction.Axis axis = oldState.hasProperty(PhaseRelayBlock.AXIS)
-                        ? oldState.getValue(PhaseRelayBlock.AXIS)
-                        : Direction.Axis.Y;
-
-                BlockState newState = ModBlocks.PHASE_RELAY.get().defaultBlockState()
-                        .setValue(PhaseRelayBlock.AXIS, axis);
-                level.setBlock(pos, newState, Block.UPDATE_ALL);
-            }
-
-            if (level.getBlockEntity(pos) instanceof PhaseRelayBlockEntity relay) {
-                relay.tuneChannel(channel, true);
-                level.playSound(null, pos, SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS, 1.0F, 0.8F);
-            }
+            BlockState newState = ModBlocks.PHASE_RELAY.get().defaultBlockState()
+                    .setValue(PhaseRelayBlock.AXIS, axis);
+            level.setBlock(targetPos, newState, Block.UPDATE_ALL);
         }
 
-        @Override
-        public void cast(CastingEnvironment ctx, CastingImage image) {
-            cast(ctx);
+        if (level.getBlockEntity(targetPos) instanceof PhaseRelayBlockEntity relay) {
+            relay.tuneChannel(channel, true);
+            level.playSound(null, targetPos, SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS, 1.0F, 0.8F);
         }
+
+        CastingImage newImage = image.copy(stack, image.getParenChildren(), image.getEscapeNext(), image.getOpsConsumed() + 1, image.getUserData());
+        return new OperationResult(newImage, List.of(ParticleSpray.cloud(Vec3.atCenterOf(targetPos), 1.5D, 20)), continuation, null);
     }
 }
