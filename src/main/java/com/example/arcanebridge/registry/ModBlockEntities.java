8fe8a0501019463b90b0d682c59db8a9fee6791a
golden <arcane_assamblage@mail.ru>
2026-08-25 14:06:49 +0300

    ⚡ Auto-Patch: ModBlockEntities.java - file created/overwritten

---
+++ src/main/java/com/example/arcanebridge/registry/ModBlockEntities.java
@@ -0,0 +1,21 @@
+package com.example.arcanebridge.registry;
+
+import com.example.arcanebridge.block.entity.PhaseRelayBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockEntities {
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
+            DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, "arcane_bridge");
+
+    public static final RegistryObject<BlockEntityType<PhaseRelayBlockEntity>> PHASE_RELAY =
+            BLOCK_ENTITIES.register("phase_relay",
+                    () -> BlockEntityType.Builder.of(PhaseRelayBlockEntity::new, ModBlocks.PHASE_RELAY.get()).build(null));
+
+    public static void register(IEventBus bus) {
+        BLOCK_ENTITIES.register(bus);
+    }
+}
