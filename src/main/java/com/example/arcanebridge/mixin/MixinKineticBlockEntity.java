27301db2e67b38225969c5adc52307757f0a890c
golden <arcane_assamblage@mail.ru>
2026-08-25 14:01:37 +0300

    ⚡ Auto-Patch: MixinKineticBlockEntity.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/mixin/MixinKineticBlockEntity.java
+++ src/main/java/com/example/arcanebridge/mixin/MixinKineticBlockEntity.java
@@ -3,52 +3,44 @@ package com.example.arcanebridge.mixin;
 import com.example.arcanebridge.hex.network.PhaseNetworkManager;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = KineticBlockEntity.class, remap = false)
-public abstract class MixinKineticBlockEntity extends BlockEntity {
-
-    public MixinKineticBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
-    }
+@Mixin(KineticBlockEntity.class)
+public abstract class MixinKineticBlockEntity {
 
     /**
      * 1. Вал-приемник объявляет Create скорость генерации (RPM)
      */
-    @Inject(method = "getGeneratedSpeed", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getGeneratedSpeed", at = @At("HEAD"), cancellable = true, remap = false)
     private void arcaneBridge$getGeneratedSpeed(CallbackInfoReturnable<Float> cir) {
-        if (this.level != null && !this.level.isClientSide) {
-            MinecraftServer server = this.level.getServer();
-            if (server != null) {
-                PhaseNetworkManager manager = PhaseNetworkManager.get(server);
-                PhaseNetworkManager.PhaseChannel channel = manager.getChannelByReceiver(this.level.dimension(), this.worldPosition);
-                if (channel != null && Math.abs(channel.currentSpeed) > 0.01f) {
-                    cir.setReturnValue(channel.currentSpeed);
-                }
+        BlockEntity be = (BlockEntity) (Object) this;
+        Level level = be.getLevel();
+        if (level != null && !level.isClientSide() && level.getServer() != null) {
+            PhaseNetworkManager manager = PhaseNetworkManager.get(level.getServer());
+            PhaseNetworkManager.PhaseChannel channel = manager.getChannelByReceiver(level.dimension(), be.getBlockPos());
+            if (channel != null && Math.abs(channel.currentSpeed) > 0.01f) {
+                cir.setReturnValue(channel.currentSpeed);
             }
         }
     }
 
     /**
-     * 2. Вал-приемник транслирует доступную мощность (SU) от генераторов передатчика
+     * 2. Вал-приемник выделяет доступную мощность (Stress Capacity) для сети
      */
-    @Inject(method = "calculateAddedStressCapacity", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "calculateAddedStressCapacity", at = @At("HEAD"), cancellable = true, remap = false)
     private void arcaneBridge$calculateAddedStressCapacity(CallbackInfoReturnable<Float> cir) {
-        if (this.level != null && !this.level.isClientSide) {
-            MinecraftServer server = this.level.getServer();
-            if (server != null) {
-                PhaseNetworkManager manager = PhaseNetworkManager.get(server);
-                PhaseNetworkManager.PhaseChannel channel = manager.getChannelByReceiver(this.level.dimension(), this.worldPosition);
-                if (channel != null && Math.abs(channel.currentSpeed) > 0.01f) {
-                    cir.setReturnValue(channel.txCapacity > 0 ? channel.txCapacity : 102400.0f);
-                }
+        BlockEntity be = (BlockEntity) (Object) this;
+        Level level = be.getLevel();
+        if (level != null && !level.isClientSide() && level.getServer() != null) {
+            PhaseNetworkManager manager = PhaseNetworkManager.get(level.getServer());
+            PhaseNetworkManager.PhaseChannel channel = manager.getChannelByReceiver(level.dimension(), be.getBlockPos());
+            if (channel != null && Math.abs(channel.currentSpeed) > 0.01f) {
+                cir.setReturnValue(channel.txCapacity > 0 ? channel.txCapacity : 102400.0f);
             }
         }
     }
@@ -56,16 +48,15 @@ public abstract class MixinKineticBlockEntity extends BlockEntity {
     /**
      * 3. Вал-передатчик забирает нагрузку удаленной сети станков
      */
-    @Inject(method = "calculateStressApplied", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "calculateStressApplied", at = @At("HEAD"), cancellable = true, remap = false)
     private void arcaneBridge$calculateStressApplied(CallbackInfoReturnable<Float> cir) {
-        if (this.level != null && !this.level.isClientSide) {
-            MinecraftServer server = this.level.getServer();
-            if (server != null) {
-                PhaseNetworkManager manager = PhaseNetworkManager.get(server);
-                PhaseNetworkManager.PhaseChannel channel = manager.getChannelByTransmitter(this.level.dimension(), this.worldPosition);
-                if (channel != null && channel.rxStress > 0.0f) {
-                    cir.setReturnValue(channel.rxStress);
-                }
+        BlockEntity be = (BlockEntity) (Object) this;
+        Level level = be.getLevel();
+        if (level != null && !level.isClientSide() && level.getServer() != null) {
+            PhaseNetworkManager manager = PhaseNetworkManager.get(level.getServer());
+            PhaseNetworkManager.PhaseChannel channel = manager.getChannelByTransmitter(level.dimension(), be.getBlockPos());
+            if (channel != null && channel.rxStress > 0.0f) {
+                cir.setReturnValue(channel.rxStress);
             }
         }
     }
