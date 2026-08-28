5d9bd648c98b59bf8af2a8a3fac39909258788ad
golden <arcane_assamblage@mail.ru>
2026-08-28 20:06:31 +0300

    ⚡ Auto-Patch: PhaseFluidBlock.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/block/PhaseFluidBlock.java
+++ src/main/java/com/example/arcanebridge/block/PhaseFluidBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -22,8 +23,12 @@ public class PhaseFluidBlock extends Block implements IBE<PhaseFluidBlockEntity>
     }
 
     @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new PhaseFluidBlockEntity(pos, state);
+    }
+
+    @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        // Поддержка перелива жидкостей ведрами и колбами по ПКМ
         if (FluidUtil.interactWithFluidHandler(player, hand, level, pos, hit.getDirection())) {
             return InteractionResult.sidedSuccess(level.isClientSide());
         }
