d1be268b6c15579afa13dd9e8408021d11556316
golden <arcane_assamblage@mail.ru>
2026-08-28 19:50:18 +0300

    ⚡ Auto-Patch: ModCommonEvents.java - applied 1 patches

--- src/main/java/com/example/arcanebridge/event/ModCommonEvents.java
+++ src/main/java/com/example/arcanebridge/event/ModCommonEvents.java
@@ -1,7 +1,11 @@
 package com.example.arcanebridge.event;
 
+import com.example.arcanebridge.fluid.PhaseFluidCapabilityProvider;
 import com.example.arcanebridge.fluid.PhaseFluidSavedData;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,10 +13,17 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = "arcane_bridge")
 public class ModCommonEvents {
 
+    private static final ResourceLocation PHASE_FLUID_CAP_KEY = new ResourceLocation("arcane_bridge", "phase_fluid_capability");
+
     @SubscribeEvent
     public static void onLevelLoad(LevelEvent.Load event) {
         if (event.getLevel() instanceof ServerLevel serverLevel && serverLevel.dimension() == ServerLevel.OVERWORLD) {
             PhaseFluidSavedData.init(serverLevel);
         }
+    }
+
+    @SubscribeEvent
+    public static void onAttachCapabilities(AttachCapabilitiesEvent<BlockEntity> event) {
+        event.addCapability(PHASE_FLUID_CAP_KEY, new PhaseFluidCapabilityProvider(event.getObject()));
     }
 }
