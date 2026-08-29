e53d0ab0c8dce06773ff73bfa78361ba4441606d
golden <arcane_assamblage@mail.ru>
2026-08-30 02:27:09 +0300

    ⚡ Auto-Patch: NetworkHandler.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/network/NetworkHandler.java
+++ src/main/java/com/example/arcanebridge/network/NetworkHandler.java
@@ -47,14 +47,14 @@ public class NetworkHandler {
                 .consumerMainThread(ServerboundRaidEndPacket::handle)
                 .add();
 
-                // 5. Завершение калибровки брони (GUI -> Сервер)
+        // 5. Завершение калибровки брони (GUI -> Сервер)
         CHANNEL.messageBuilder(ServerboundRepairCompletePacket.class, packetId++, NetworkDirection.PLAY_TO_SERVER)
                 .encoder(ServerboundRepairCompletePacket::encode)
                 .decoder(ServerboundRepairCompletePacket::decode)
                 .consumerMainThread(ServerboundRepairCompletePacket::handle)
                 .add();
 
-                // 6. Запрос действий Гида / Локатора (GUI -> Сервер)
+        // 6. Запрос действий Гида / Локатора (GUI -> Сервер)
         CHANNEL.messageBuilder(ServerboundGuideActionPacket.class, packetId++, NetworkDirection.PLAY_TO_SERVER)
                 .encoder(ServerboundGuideActionPacket::toBytes)
                 .decoder(ServerboundGuideActionPacket::new)
@@ -66,6 +66,13 @@ public class NetworkHandler {
                 .encoder(ClientboundPhaseFluidSyncPacket::toBytes)
                 .decoder(ClientboundPhaseFluidSyncPacket::new)
                 .consumerMainThread(ClientboundPhaseFluidSyncPacket::handle)
+                .add();
+
+        // 8. Синхронизация NBT щитов мобов (Сервер -> Клиент)
+        CHANNEL.messageBuilder(ClientboundSyncShieldPacket.class, packetId++, NetworkDirection.PLAY_TO_CLIENT)
+                .encoder(ClientboundSyncShieldPacket::encode)
+                .decoder(ClientboundSyncShieldPacket::decode)
+                .consumerMainThread(ClientboundSyncShieldPacket::handle)
                 .add();
     }
 
