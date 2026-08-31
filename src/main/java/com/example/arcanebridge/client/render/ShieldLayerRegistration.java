e7d2e9f854a424d51dee21c0476488ab8f55873a
golden <arcane_assamblage@mail.ru>
2026-08-31 11:51:06 +0300

    ⚡ Auto-Patch: ShieldLayerRegistration.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/render/ShieldLayerRegistration.java
+++ src/main/java/com/example/arcanebridge/client/render/ShieldLayerRegistration.java
@@ -9,7 +9,8 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import software.bernie.geckolib.renderer.GeoRenderer;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.GeoReplacedEntityRenderer;
 
 @Mod.EventBusSubscriber(modid = MobArchetypes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ShieldLayerRegistration {
@@ -17,17 +18,21 @@ public class ShieldLayerRegistration {
     @SubscribeEvent
     @SuppressWarnings({"unchecked", "rawtypes"})
     public static void onAddLayers(EntityRenderersEvent.AddLayers event) {
+        // Регистрация слоев для всех зарегистрированных сущностей мира
         for (EntityType<?> type : BuiltInRegistries.ENTITY_TYPE) {
             try {
                 EntityRenderer<?> renderer = event.getRenderer((EntityType) type);
                 if (renderer instanceof LivingEntityRenderer livingRenderer) {
                     livingRenderer.addLayer(new ShieldSuitLayer(livingRenderer));
-                } else if (renderer instanceof GeoRenderer geoRenderer) {
+                } else if (renderer instanceof GeoEntityRenderer geoRenderer) {
                     geoRenderer.addRenderLayer(new GeoShieldSuitLayer(geoRenderer));
+                } else if (renderer instanceof GeoReplacedEntityRenderer geoReplacedRenderer) {
+                    geoReplacedRenderer.addRenderLayer(new GeoShieldSuitLayer(geoReplacedRenderer));
                 }
             } catch (Throwable ignored) {}
         }
 
+        // Регистрация слоя для скинов игроков (на случай PvP)
         for (String skin : event.getSkins()) {
             try {
                 LivingEntityRenderer playerRenderer = event.getSkin(skin);
