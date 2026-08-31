6bbb33463ed20b325f28e5e87e559e235e2d7157
golden <arcane_assamblage@mail.ru>
2026-08-31 12:04:33 +0300

    ⚡ Auto-Patch: ShieldLayerRegistration.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/render/ShieldLayerRegistration.java
+++ src/main/java/com/example/arcanebridge/client/render/ShieldLayerRegistration.java
@@ -3,43 +3,68 @@ package com.example.arcanebridge.client.render;
 import com.example.arcanebridge.combat.MobArchetypes;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import software.bernie.geckolib.event.GeoRenderEvent;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.renderer.GeoReplacedEntityRenderer;
 
-@Mod.EventBusSubscriber(modid = MobArchetypes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+import java.lang.reflect.Field;
+import java.util.Map;
+
 public class ShieldLayerRegistration {
 
-    @SubscribeEvent
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public static void onAddLayers(EntityRenderersEvent.AddLayers event) {
-        // Регистрация слоев для всех зарегистрированных сущностей мира
-        for (EntityType<?> type : BuiltInRegistries.ENTITY_TYPE) {
-            try {
-                EntityRenderer<?> renderer = event.getRenderer((EntityType) type);
-                if (renderer instanceof LivingEntityRenderer livingRenderer) {
-                    livingRenderer.addLayer(new ShieldSuitLayer(livingRenderer));
-                } else if (renderer instanceof GeoEntityRenderer geoRenderer) {
-                    geoRenderer.addRenderLayer(new GeoShieldSuitLayer(geoRenderer));
-                } else if (renderer instanceof GeoReplacedEntityRenderer geoReplacedRenderer) {
-                    geoReplacedRenderer.addRenderLayer(new GeoShieldSuitLayer(geoReplacedRenderer));
-                }
-            } catch (Throwable ignored) {}
-        }
+    // 1. Регистрация на шине MOD: Ванильные сущности и прямой доступ к GeckoLib рендерерам
+    @Mod.EventBusSubscriber(modid = MobArchetypes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ModBusEvents {
 
-        // Регистрация слоя для скинов игроков (на случай PvP)
-        for (String skin : event.getSkins()) {
+        @SubscribeEvent
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        public static void onAddLayers(EntityRenderersEvent.AddLayers event) {
             try {
-                LivingEntityRenderer playerRenderer = event.getSkin(skin);
-                if (playerRenderer != null) {
-                    playerRenderer.addLayer(new ShieldSuitLayer(playerRenderer));
+                // Извлекаем внутреннюю карту рендереров Forge напрямую в обход ClassCastException
+                Field renderersField = EntityRenderersEvent.AddLayers.class.getDeclaredField("renderers");
+                renderersField.setAccessible(true);
+                Map<EntityType<?>, EntityRenderer<?>> renderers = (Map<EntityType<?>, EntityRenderer<?>>) renderersField.get(event);
+
+                if (renderers != null) {
+                    for (EntityRenderer<?> renderer : renderers.values()) {
+                        if (renderer instanceof LivingEntityRenderer livingRenderer) {
+                            livingRenderer.addLayer(new ShieldSuitLayer(livingRenderer));
+                        } else if (renderer instanceof GeoEntityRenderer geoRenderer) {
+                            geoRenderer.addRenderLayer(new GeoShieldSuitLayer(geoRenderer));
+                        } else if (renderer instanceof GeoReplacedEntityRenderer geoReplacedRenderer) {
+                            geoReplacedRenderer.addRenderLayer(new GeoShieldSuitLayer(geoReplacedRenderer));
+                        }
+                    }
                 }
-            } catch (Throwable ignored) {}
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            // Подключение слоя к скинам игроков
+            for (String skin : event.getSkins()) {
+                try {
+                    LivingEntityRenderer playerRenderer = event.getSkin(skin);
+                    if (playerRenderer != null) {
+                        playerRenderer.addLayer(new ShieldSuitLayer(playerRenderer));
+                    }
+                } catch (Throwable ignored) {}
+            }
+        }
+    }
+
+    // 2. Регистрация на шине FORGE: Нативный перехват компиляции слоев GeckoLib 4
+    @Mod.EventBusSubscriber(modid = MobArchetypes.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    public static class ForgeBusEvents {
+
+        @SubscribeEvent
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        public static void onGeckoCompileRenderLayers(GeoRenderEvent.Entity.CompileRenderLayers event) {
+            event.addLayer(new GeoShieldSuitLayer(event.getRenderer()));
         }
     }
 }
