866a8fa09c0727f7ebe09565c869b477cd42a89b
golden <arcane_assamblage@mail.ru>
2026-08-31 11:46:47 +0300

    ⚡ Auto-Patch: ShieldLayerRegistration.java - file created/overwritten

--- src/main/java/com/example/arcanebridge/client/render/ShieldLayerRegistration.java
+++ src/main/java/com/example/arcanebridge/client/render/ShieldLayerRegistration.java
@@ -1,6 +1,7 @@
 package com.example.arcanebridge.client.render;
 
 import com.example.arcanebridge.combat.MobArchetypes;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
@@ -8,6 +9,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import software.bernie.geckolib.renderer.GeoRenderer;
 
 @Mod.EventBusSubscriber(modid = MobArchetypes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ShieldLayerRegistration {
@@ -15,17 +17,17 @@ public class ShieldLayerRegistration {
     @SubscribeEvent
     @SuppressWarnings({"unchecked", "rawtypes"})
     public static void onAddLayers(EntityRenderersEvent.AddLayers event) {
-        // Подключаем скафандр ко всем зарегистрированным сущностям
         for (EntityType<?> type : BuiltInRegistries.ENTITY_TYPE) {
             try {
-                LivingEntityRenderer renderer = event.getRenderer((EntityType) type);
-                if (renderer != null) {
-                    renderer.addLayer(new ShieldSuitLayer(renderer));
+                EntityRenderer<?> renderer = event.getRenderer((EntityType) type);
+                if (renderer instanceof LivingEntityRenderer livingRenderer) {
+                    livingRenderer.addLayer(new ShieldSuitLayer(livingRenderer));
+                } else if (renderer instanceof GeoRenderer geoRenderer) {
+                    geoRenderer.addRenderLayer(new GeoShieldSuitLayer(geoRenderer));
                 }
             } catch (Throwable ignored) {}
         }
 
-        // Подключаем к скинам игроков на случай дуэлей / PVP
         for (String skin : event.getSkins()) {
             try {
                 LivingEntityRenderer playerRenderer = event.getSkin(skin);
